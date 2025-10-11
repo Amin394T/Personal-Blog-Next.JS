cc5f4a19755c001ab7610df28bf8c5d1ae1c0cba
Amin394 <amin394@outlook.com>
2025-10-11 14:50:48 +0100

    improved logic in user modifcation route

--- app/api/users/modify/route.ts
+++ app/api/users/modify/route.ts
@@ -1,27 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/database/userModel";
+import Message from "@/database/messageModel";
+
 
 export const PATCH = async (req: NextRequest) => {
   let { id, username, password, status, token } = await req.json();
   
   if (token != process.env.ADMIN_TOKEN)
-        return NextResponse.json({ code: 71, message: "Access Forbidden!" }, { status: 401 });
+    return NextResponse.json({ code: 71, message: "Access Forbidden!" }, { status: 401 });
 
   try {
     const user: any = await User.findByPk(id);
     if (!user)
       return NextResponse.json({ code: 72, message: "User not Found!" }, { status: 404 });
 
-    user.username = username || user.username;
-    user.password = password || user.password;
-    user.status = status || user.status;
-    await user.save();
+    await User.update(
+      { username, password, status },
+      { where: { username: id } }
+    );
 
-    return NextResponse.json(user, { status: 200 });
+    await Message.update(
+      { user: username },
+      { where: { user: id } }
+    );
+
+    return NextResponse.json({ code: 79, message: "User Modified."}, { status: 200 });
   }
   catch (error: any) {
     return NextResponse.json({ code: 70, message: error.message }, { status: 500 });
   }
 };
-
-// modify all comments with new name
